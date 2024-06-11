4922e9ea16d1af64e154c7bfeb927eb210a3fef2
Qusai Kader <qusai.kader@somaiya.edu>
2024-06-11 21:49:09 +0530

    Added concept of routes and fallback error page

--- Working-With-ExpressJs/app.js
+++ Working-With-ExpressJs/app.js
@@ -1,9 +1,20 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 const app = express();
 
-app.use((req, res, next) => {
-    res.send('<h1>Hello from express</h1>');
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(adminRoutes);
+
+app.use(shopRoutes);
+
+app.use('/', (req, res) => {
+    res.status(404).send('<h1>404 Page Not Found</h1>');
 });
 
-app.listen(3000);
+app.listen(3000, () =>{
+    console.log('Server started at port 3000');
+});
